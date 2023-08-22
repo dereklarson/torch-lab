c4a50fc62499c36255138372331435fadbfaa4c8
Derek Larson <larson.derek.a@gmail.com>
2023-08-22 16:11:54 -0700

    Expose all base classes in core init.py

--- tlab/__init__.py
+++ tlab/__init__.py
@@ -1,4 +1,6 @@
+from tlab.datasets import LabDataset
 from tlab.experiment import Experiment
+from tlab.models import LabModel
 from tlab.observation import Observables, Observations
 from tlab.optimizers.lab_optimizer import LabOptimizer
 from tlab.xconfiguration import XConfiguration
--- tlab/models/__init__.py
+++ tlab/models/__init__.py
@@ -1,3 +1,4 @@
 from tlab.models.conv import ConvNet, LeNet
+from tlab.models.lab_model import LabModel
 from tlab.models.mlp import MLP, EmbedMLP
 from tlab.models.transformer import Transformer
